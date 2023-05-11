f661310bb0a23825d6b81af0ba7224a64643db10
Faizan-92 <faizanfmemon@gmail.com>
2023-05-11 10:37:24 +0530

    task: initial project setup and dependencies included

---
+++ Disneyverse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Disneyverse
+//
+//  Created by Faizan Memon on 11/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
